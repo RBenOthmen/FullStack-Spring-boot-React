a2245e9f6173d00d04aa0886e49b2ecf1e284f6e
rami ben othmen <ramibenothmen@gmail.com>
2022-07-10 16:21:16 +0100

    Lesson 10

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import "./App.css";
-import { getAllStudents } from './client';
+import { getAllStudents } from "./client";
 
 class App extends Component {
+  state = { students: [] };
+
+  componentDidMount() {
+    this.fetchStudents();
+  }
+
+  fetchStudents = () => {
+    getAllStudents().then((res) =>
+      res.json().then((students) => {
+        console.log(students);
+        this.setState({ students });
+      })
+    );
+  };
+
   render() {
-    getAllStudents().then(res => res.json().then(students => {
-      console.log(students);
-    }));
-    return <h1>Hello world spring boot & react</h1>
+    const { students } = this.state;
+
+    if (students && students.length) {
+      return students.map((student, index) => {
+        return (
+          <div key={index}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+            <p>{student.email}</p>
+          </div>
+        );
+      });
+    }
+    return <h1>No Students found</h1>;
   }
 }
 
